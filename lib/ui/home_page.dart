afeb8bb6fec753e0686bf64ff50eb27f3574e0e4
isacmaximo <isacmaximomoura@gmail.com>
2021-09-15 15:22:00 -0300

    Top 20 gifs homepage

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -15,35 +15,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   //variável de pesquisa:
   //colocamos interrogação pois estão com valor inicial null
   String? _search;
+
   //vai representar a quantidade de páginas
   int? _offset;
 
   //função que vai fazer as requisições de busca dos gifs:
   //como não é instantâneo, usamos async:
-  Future<Map> _getGifs() async{
+  Future<Map> _getGifs() async {
     http.Response response;
     //no caso vai ter dois tipos de resposta: top gis e pesquisa
 
     //se na pesquisa não estiver nada, então os top 20 gifs aparecerão:
-    if(_search == null){
-      response = await http.get(Uri.parse("https://api.giphy.com/v1/gifs/trending?api_key=qPx7EIu7Jk8dAwWIb57xNBHkO6QQcP7w&limit=20&rating=g"));
+    if (_search == null) {
+      response = await http.get(Uri.parse(
+          "https://api.giphy.com/v1/gifs/trending?api_key=qPx7EIu7Jk8dAwWIb57xNBHkO6QQcP7w&limit=20&rating=g"));
     }
     //no caso se for uma pesquisa:
-    else{
+    else {
       //parâmetros da busca: (q = _search) | (offset = _offset)
-      response = await http.get(Uri.parse("https://api.giphy.com/v1/gifs/search?api_key=qPx7EIu7Jk8dAwWIb57xNBHkO6QQcP7w&q=$_search&limit=20&offset=$_offset&rating=g&lang=en"));
+      response = await http.get(Uri.parse(
+          "https://api.giphy.com/v1/gifs/search?api_key=qPx7EIu7Jk8dAwWIb57xNBHkO6QQcP7w&q=$_search&limit=20&offset=$_offset&rating=g&lang=en"));
     }
 
     //precisamos requisitar o arquivo no formato json:
     return json.decode(response.body);
-
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +52,8 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.black,
         //o título do AppBar pode ser uma imagem também, não precisa ser um texto
         //no caso vamos pegar uma imagem da internet:
-        title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
         centerTitle: true,
       ),
 
@@ -62,18 +62,85 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
                 decoration: InputDecoration(
                     labelText: "Pesquise Aqui!",
                     labelStyle: TextStyle(color: Colors.white),
-                    border: OutlineInputBorder()
-                ),
-                style: TextStyle(color: Colors.white,fontSize: 18.0),
+                    border: OutlineInputBorder()),
+                style: TextStyle(color: Colors.white, fontSize: 18.0),
                 textAlign: TextAlign.center,
-          ))
+              )
+          ),
+
+          //grade de gifs: como queremos que ocupe o resto da homepage, usaremos um expanded:
+          Expanded(
+              child: FutureBuilder(
+                future: _getGifs(),
+                //verifica o estado:
+                builder: (context, snapshot){
+                  switch (snapshot.connectionState){
+                    //primeiro caso: esteja esperando por algo
+                    case ConnectionState.waiting:
+                      //segundo caso: esteja esperando por nada
+                    case ConnectionState.none:
+                      //aqui vai mostrar o widget de carregamento:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        //tipo circular:
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
+                        ),
+                      );
+                    default:
+                      //se o snapshot tem um erro:
+                      if (snapshot.hasError){
+                        return Container();
+                      }
+                      //se não, ele carrega a tabela de gifs
+                      else{
+                        return _createGifTable(context, snapshot);
+                      }
+                  }
+                }
+              ),
+          ),
         ],
       ),
     );
   }
+
+  //grade de gifs:
+Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+    //retornando um widget gridview
+  return GridView.builder(
+    padding: EdgeInsets.all(10.0),
+
+    //como ops ítens vão ser organizados na tela:
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //quantos ítens ele vai ter na horizontal:
+      crossAxisCount: 2,
+      //espaçamento entre os ítens na horizontal
+      crossAxisSpacing: 10.0,
+      //espaçamento na vertical
+      mainAxisSpacing: 10.0,
+    ),
+
+    itemCount: snapshot.data["data"].length,
+
+    itemBuilder: (context, index){
+      //aqui vai retornar o ítem que aparece em cada posição:
+      //gesture detector serve para interagir (dectecta se é tocado por exemplo)
+      return GestureDetector(
+        //imagem da internet que no caso vai ser o gif:
+        child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"], height: 300.0, fit: BoxFit.cover,),
+      );
+
+    },
+  );
+}
+
 }
